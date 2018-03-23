a42a67acb3ad297d35c3c592ff195733c6a7151c
Vinicius-asf <viniciusant3@gmail.com>
2018-03-23 00:10:40 -0300

    First AP completed! LoopMusical with pikeFinder function completed!

--- PA/loopmusical/loopmusical.c
+++ PA/loopmusical/loopmusical.c
@@ -1,62 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void pikeFinder(int diff, int* pkcounter, int* variation){
-    //printf("%d",diff);
-    if (*variation == 0){
-        if (diff>0){
-            *variation = 1;
-        }
-        else{
-            *variation = -1;
-        }
+void pikeFinder(int H1, int H2, int H3, int* pkcounter){ //função que checa se os valores H1 e H3 são ambos maiores ou menores que H2. 
+    if (H2>H3 && H2>H1){                                 //Caso sejam, adiciona 1 ao contador de picos
         *pkcounter+=1;
     }
-    else{
-        if (diff>0 && variation<0){
-            *pkcounter += 1;
-            *variation = 1;
-        }
-        else if (diff<0 && variation>0){
-            *pkcounter += 1;
-            *variation = -1;
-        }
+    else if (H2<H3 && H2<H1){
+        *pkcounter+=1;
     }
-    /* if (Hi2>Hi3 && Hi2>Hi1){
-        *pkcounter += 1;
-    }
-    else if(Hi2<Hi3 && Hi2<Hi1){
-        *pkcounter += 1;
-    } */
 }
 
 int main(){
-    int N = 0, i = 0;
-    int loop[10000];
-    int pcounter = 0, vrtn = 0;
-    //printf("Antes do while\n");
+    int N = 0, i = 0; //N é o inteiro que fala quantos pontos tem a música, i é o inteiro controle do loop interno
+    int loop[10000]; //vetor que armazena os valores dos pontos da música
+    int pcounter = 0; // inteiro que armazena a quantidade de picos encontrados
 
-    while(scanf("%d",&N) && N!=0){
+    while(scanf("%d",&N) && N!=0){ //laço que acaba quando a entrada de pontos for igual a zero
         pcounter = 0;
-        vrtn = 0;
-        //printf("Antes do for\n");
-        for (i=0;N-i>0;i++){
+        for (i=0;N-i>0;i++){ //pega todos os valores de entrada e os colocam no vetor "loop"
             scanf("%d",&loop[i]);
-            //printf("%d",loop[i]);           
         }
         for (i=0;N-i>0;i++){
-            printf("%d,%d,%d",loop[i],loop[i+1],loop[i]-loop[i+1]);
-            printf("Rodando: contador = %d variacao = %d\n",pcounter,vrtn);            
             if (i==N-1){
-                pikeFinder(loop[i]-loop[0],&pcounter,&vrtn);
+                pikeFinder(loop[i-1],loop[i],loop[0],&pcounter); //chamando a função que checa os picos no inicio do loop
             }
+            else if (i==0){
+                pikeFinder(loop[N-1],loop[i],loop[i+1],&pcounter);} // chamando a função que checa os picos no final do loop
             else{
-                pikeFinder(loop[i]-loop[i+1],&pcounter,&vrtn);
+                pikeFinder(loop[i-1],loop[i],loop[i+1],&pcounter); // chamando a função que checa os picos no meio do loop
             }
-            printf("Rodando: contador = %d variacao = %d\n",pcounter,vrtn);
         }
-        //pikeFinder(loop[i-1]-loop[0],&pcounter,&vrtn);
-        printf("%d\n",pcounter);
+        printf("%d\n",pcounter); //mostrando o valor do contador de picos
     }
     return 0;
 }
