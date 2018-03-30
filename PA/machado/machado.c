948ce98ccf96a15a9bcea7ffec316304cdf3b738
Vinicius-asf <viniciusant3@gmail.com>
2018-03-30 16:12:48 -0300

    PA Machado begins

---
+++ PA/machado/machado.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    //N (2 ≤ N ≤ 1000), L (1 ≤ L ≤ 30 ) e C (1 ≤ C ≤ 70)
+    int N = 0; //# de palavras no conto
+    int L = 0; //# máximo de linhas por página
+    int C = 0; //# máximo de caracteres por linha
+    char palavra [70];
+    while (scanf("%d",&N) && N!='EOF'){
+        scanf("%d%d",&L,&C);
+        gets();
+    }
+}
