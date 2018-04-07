544a8559fabf991d03edfde4356e2e5677975551
Vinicius-asf <viniciusant3@gmail.com>
2018-04-07 18:44:45 -0300

    getValor function created. Testing int main.c

--- TP/TPZERO/grafo.h
+++ TP/TPZERO/grafo.h
@@ -7,30 +7,11 @@ typedef struct modlista {
         int id;
         int valor;
         struct modlista *prox;           //ponteiro para o proximo elemento
-}**vetor, lista, *elo;                 //um vetor de ponteiro para a struct, a estrutura, ponteiro para a struct
+}**vetor, lista, *elo;                 //um ponteiro duplo de vetor, a estrutura e o ponteiro para a struct
 
 
+void inicializevetor(vetor v, int pvalor, int index);
 
-void inicializevetor(vetor v, int pvalor, int index){
-        elo nucleo;
-        nucleo->prox=NULL;
-        nucleo->id=index;
-        nucleo->valor=pvalor;
-        v[index] = nucleo;
-}
+void createElo(vetor v, int pindex, int nindex, int nvalor);
 
-void createElo(vetor v, int pindex, int nindex, int nvalor){
-        elo novo, aux;
-        aux=v[pindex];
-        while(aux->prox!=NULL){
-                aux=aux->prox;
-        }
-        aux->prox=novo;
-        aux=v[novo->id];
-        while(aux->prox!=NULL){
-                aux=aux->prox;
-        }
-        aux->prox=v[pindex];
-        
-}
-
+int getValor(vetor v, int index);
--- TP/TPZERO/main.c
+++ TP/TPZERO/main.c
@@ -1,1 +1,10 @@
 #include "grafo.h"
+
+int main (){
+    vetor v;
+    inicializevetor(v,1,1);
+    inicializevetor(v,2,2);
+
+    print("%d : %d",getValor(v,1),getValor(v,2));
+    //createElo(v,1,2,2);
+}
