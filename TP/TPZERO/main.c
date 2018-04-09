541b9d64af65d4a743dd97478d453a3861a229b0
Vinicius-asf <viniciusant3@gmail.com>
2018-04-09 18:14:20 -0300

    modifications into main.c

--- TP/TPZERO/main.c
+++ TP/TPZERO/main.c
@@ -20,12 +20,5 @@ int main (){
 
     destroyList(lista);
     
-    
-    /* graphL_pont grafo = createGraphL(3);
-    addAresta(grafo,0,1);
-    addAresta(grafo,0,2);
-    addAresta(grafo,1,2);
-    printGraphL(grafo);
-    destroyGraphL(grafo); */
     return 0;
 }
