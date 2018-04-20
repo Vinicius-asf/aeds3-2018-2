00cfceba338ea337558fcef45fa2c77dd1ad608f
Vinicius <viniciusant3@gmail.com>
2018-04-20 03:44:48 +0000

    Ended the sentença dançante!

--- PA/sentencadancante/sentencadancante.c
+++ PA/sentencadancante/sentencadancante.c
@@ -1,36 +1,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void uppercase(char* letter){
-    *letter-=32;
+void uppercase(char *letter)
+{
+    *letter -= 32;
 }
 
-void lowercase(char* letter){
-    *letter+=32;
+void lowercase(char *letter)
+{
+    *letter += 32;
 }
 
-int main () {
-    char teste[50];
+int main()
+{
+    char teste[51];
     int i = 0;
     int Case = 0;
-    fgets(teste,50,stdin);
-    while (teste[i]!=10){
-        if (teste[i]!=32){
-            if (Case == 0){
-                Case = 1;
-                if (teste[i] > 96){
-                    uppercase(&teste[i]);
+    while (fgets(teste, 51, stdin) && teste[i] != 0)
+    {
+        while (teste[i] != 10)
+        {
+            if (teste[i] != 32)
+            {
+                if (Case == 0)
+                {
+                    Case = 1;
+                    if (teste[i] > 96)
+                    {
+                        uppercase(&teste[i]);
+                    }
+                }
+                else
+                {
+                    Case = 0;
+                    if (teste[i] < 96)
+                    {
+                        lowercase(&teste[i]);
+                    }
                 }
             }
-            else{
-                Case = 0;
-                if (teste[i] < 96){
-                    lowercase(&teste[i]);
-                }
-            }
+            i++;
         }
-        i++;
-    }
-    printf("%s",teste);
+        printf("%s", teste);
+        i = 0;
+        Case = 0;
+    } 
     return 0;
 }
